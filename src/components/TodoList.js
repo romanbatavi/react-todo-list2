4713ee918241fa7320aac7e86107add2923ce918
romanbatavi <romanbatavi98@gmail.com>
2021-04-15 19:59:01 +0700

    todos, function, geticons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,12 +14,26 @@ function TodoList() {
         setTodos(newTodos)
     };
 
+    const completeTodo = id => {
+        let updateTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        });
+        setTodos(updateTodos);
+    };
+
     return (
         <div>
             <h1>Whats Ur Plan Today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo 
+            todos={todos} 
+            completeTodo={completeTodo}
+            />
         </div>
-    )
+    );
 }
 
 export default TodoList

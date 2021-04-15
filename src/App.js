6d0b7349b6a33b26ef98e3235298ccc1ee45b38a
romanbatavi <romanbatavi98@gmail.com>
2021-04-15 17:45:35 +0700

    handle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>
-        Todo App
-      </h1>
+      <TodoForm />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,33 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
-    const [input, setInput] = useState('')
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 10000),
+        //     text: input
+        // });
+
+        setInput('')
+    };
     
     return (
-        <form className="todo-form">
-            <input type="text" placeholder="Add Here" value={input} name="text" className='todo-input'/>
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            placeholder="Add Here"
+            value={input}
+            name="text"
+            className='todo-input'
+            onChange={handleChange}
+            />
             <button className="todo-button">Add Todo</button>
         </form>
     )
